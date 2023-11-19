eae802cb1173d09dd6d5fe33fb91909776a63801
dannytoy <chiisaidanny@gmail.com>
2023-11-19 15:58:10 -0700

    default-pages

--- src/pages/DannyToy.js
+++ src/pages/DannyToy.js
@@ -6,7 +6,7 @@ export default function examplePage() {
 
         <main>
           <div className='underline'>
-            <h1 className='title'>Hey, I'm a copy of Spider man(danny). </h1>
+            <h1 className='title'>Hey, I'm a copy of Spider danny-man. </h1>
           </div>
           <img
             className='pic'
